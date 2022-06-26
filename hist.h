a583d8d4d10080f916431890fb1f950058399c51
yref-boop <regofdez.yago@gmail.com>
2022-06-27 00:43:44 +0200

    chore: reorganize shell 1

--- hist.h
+++ hist.h
@@ -21,11 +21,10 @@ struct tNode { //nodes are structures composed of data and a pointer to the next
 };
 typedef tCommand_pos tHist;
 
-// headers of functions:
+//headers of functions:
 void createEmptyList (tHist* list); //creates a list that doesn't have items
 tCommand_pos first(tHist list); //returns the position of the first element of a non-empty list
 tCommand_pos next (tCommand_pos pos, tHist list); //returns the position of the following element of an item in a given position of a list
-tCommand_pos previous (tCommand_pos pos, tHist list); //returns the position of the item before the given
 bool insertItem(struct tNode node, tHist *list); // inserts item with given contents
 void deleteAtPosition(tCommand_pos pos, tHist *list); //deletes item from the list at a given position
 tCommand getItem(tCommand_pos pos, tHist list); //returns the contents of an element at a given position
